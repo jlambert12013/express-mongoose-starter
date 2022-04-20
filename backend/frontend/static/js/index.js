df6539de0760b91f7dc5ee29fb584475a02c9b94
jlambert120!# <jlambert12013@gmail.com>
2022-04-20 13:46:11 -0500

    On branch master

---
+++ backend/frontend/static/js/index.js
@@ -0,0 +1,48 @@
+const navigateTo = (url) => {
+  history.pushState(null, null, url)
+  router()
+}
+
+const router = async () => {
+  const routes = [
+    {
+      path: '/',
+      view: () => console.log('Viewing Dashbaord'),
+    },
+    {
+      path: '/posts',
+      view: () => console.log('Viewing Post'),
+    },
+    {
+      path: '/settings',
+      view: () => console.log('Viewing Settings'),
+    },
+  ]
+
+  // Test each route for match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    }
+  })
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch)
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    }
+  }
+  console.log(match.route.view())
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', (e) => {
+    if (e.target.matches('[data-link]')) {
+      e.preventDefault()
+      navigateTo(e.target.href)
+    }
+  })
+  router()
+})
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 // Required Modules
 require('dotenv').config()
 require('colors')
+
 const express = require('express')
 const app = express()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const path = require('path')
 
 // Database
 connectDB()
@@ -13,9 +15,18 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(
+  '/static',
+  express.static(path.resolve(__dirname, 'frontend', 'static'))
+)
+
 // Route
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve('frontend', 'index.html'))
+})
 app.use('/api/goals', require('./routes/goalRoutes'))
 
+//  Error Handler
 app.use(errorHandler)
 
 // Server
