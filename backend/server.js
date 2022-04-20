bf255cc15d9360ad45f38980e5c4c9b9c6f472aa
jlambert120!# <jlambert12013@gmail.com>
2022-04-20 13:19:56 -0500

    On branch master

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,23 @@
 // Required Modules
-require("dotenv").config();
-require("colors");
-const express = require("express");
-const app = express();
-const { errorHandler } = require("./middleware/errorMiddleware");
-const mongoose = require("mongoose");
-const connectDB = require("./config/db");
+require('dotenv').config()
+require('colors')
+const express = require('express')
+const app = express()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 
 // Database
-connectDB();
+connectDB()
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 // Route
-app.use("/api/goals", require("./routes/goalRoutes"));
+app.use('/api/goals', require('./routes/goalRoutes'))
 
-app.use(errorHandler);
+app.use(errorHandler)
 
 // Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`BACKEND SERVER STARTED ON PORT ${PORT}.`));
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`BACKEND SERVER STARTED ON PORT ${PORT}.`))
